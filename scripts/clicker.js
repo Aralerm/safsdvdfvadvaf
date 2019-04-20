a43bdb2d35e5a0a98481c229876b20eb098bd55b
Alexander <mrreads@yandex.com>
2019-04-20 22:50:36 +0300

    animations

--- scripts/clicker.js
+++ scripts/clicker.js
@@ -1,9 +1,8 @@
-var elem_button = document.querySelector('#button');
 var numCount = 0;
-
+var elem_button = document.querySelector('#button');
 var elem_counter = document.querySelector('#counter');
 
-// BONUS MULTIPLY
+// Инцилизируем DOM и счётчики для улучшалки кликов.
 var elem_multipluFactor = document.querySelector('#multiply_factor');
 var multiplyFactor = 1;
 var elem_multiply = document.querySelector('#multiply');
@@ -12,7 +11,7 @@ var elem_multiplyCounter = document.querySelector('#multiply_counter');
 var multiplyCost = 10;
 var elem_multiplyCost = document.querySelector('#multiply_cost');
 
-// BONUS AUTOCLICK
+// Инцилизируем DOM и счётчики для автокликера.
 var elem_autoclick = document.querySelector('#autoclick');
 var autoclickCounter = 0;
 var elem_autoclick_counter = document.querySelector('#autoclick_counter');
@@ -21,6 +20,7 @@ var elem_autoclick_cost = document.querySelector('#autoclick_cost');
 var autoclickFactor = 0;
 var elem_autoclick_factor = document.querySelector('#autoclick_factor');
 
+// Функция, которая обновляет значение всех перемен-счётчиков в HTML тегах.
 function refreshCounters ()
 {
     elem_counter.textContent = numCount.toFixed(1);
@@ -31,25 +31,40 @@ function refreshCounters ()
     elem_autoclick_cost.textContent = autoclickCost.toFixed(1);
     elem_autoclick_factor.textContent = autoclickFactor.toFixed(1);
 }
-
+// И вызываем её, чтобы отобразились стандартыне значения в браузере.
 refreshCounters();
 
-// ЕСЛИ КЛИКНУЛИ НА КНОПКУ
+// Если нажали на главную кнопку.
 elem_button.addEventListener('click', buttonClicked)
 
 function buttonClicked ()
 {
     numCount = numCount + (1 * multiplyFactor);
+    
+    // Добавяем класс, который увиличивает паддинги.
+    elem_button.classList.add('pressed');
+    // Через 100мс его убираем.
+    setTimeout (function() {
+        elem_button.classList.remove('pressed')
+    }, 100)
+    // Это мы сделали анимацию при наведении.
+
+    // Вызываем функцию, которая обновляет значение тегов с счётчиками.
     refreshCounters();
 }
 
-// ЕСЛИ НАЖАЛИ НА ПОКУПКУ УЛУЧШАЛКИ
+// Если нажали КУПИТЬ УЛУЧШЕНИЕ.
 elem_multiply.addEventListener('click', buyMultiply);
 
 function buyMultiply ()
 {
     if (numCount >= multiplyCost)
     {
+        elem_multiply.classList.add('pressed');
+        setTimeout (function() {
+            elem_multiply.classList.remove('pressed');
+        }, 100);
+
         numCount = numCount - multiplyCost;
         multiplyFactor = multiplyFactor + 0.2;
         multiplyCost = Math.round(multiplyCost * 1.5);
@@ -58,21 +73,32 @@ function buyMultiply ()
     }
 }
 
-// ЕСЛИ НАЖАЛИ НА ПОКУПКУ АВТОКЛИКЕРА
+// Если нажали КУПИТЬ АВТОКЛИКЕР.
 elem_autoclick.addEventListener('click', buyAutoclick);
 
 function buyAutoclick()
 {
     if (numCount >= autoclickCost)
     {
+        // Добавяем класс, который увиличивает паддинги.
+        elem_autoclick.classList.add('pressed');
+        // Через 100мс его убираем.
+        setTimeout (function() {
+            elem_autoclick.classList.remove('pressed');
+        }, 100);
+        // Это мы сделали анимацию при наведении.
+
         numCount = numCount - autoclickCost;
         autoclickFactor = autoclickFactor + 1;
         autoclickCost = Math.round(autoclickCost * 1.5);
         autoclickCounter = autoclickCounter + 1;
+        
+        // Вызываем функцию, которая обновляет значение тегов с счётчиками.
         refreshCounters();
     }
 }
 
+// setInterval вызывает функцию каждую секунду.
 setInterval(everySecond, 1 * 1000)
 
 function everySecond()
