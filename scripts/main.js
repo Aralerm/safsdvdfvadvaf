292f5a2b76c95d6d908ecfed32729b9d42f5ba78
Alexander <mrreads@yandex.com>
2019-09-20 20:28:52 +0300

    rewritten to class (v1)

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+let counter = 0;
+let factor = 1;
+
+document.querySelector('#button').addEventListener('click', function()
+{
+    counter = counter + (1 * factor);
+    refresh();
+});
+
+function refresh ()
+{
+    document.querySelector("#counter").textContent = counter;
+}
+
+refresh();
+
+class Bonus
+{
+    constructor(name, cost, factor, id) 
+    {
+        this.name = name;
+        this.cost = cost;
+        this.factor = factor;
+        this.own = 0;
+        this.id = id;
+
+        let tags = '<a class="bonus" id="'+id+'" href="#"> <h2>'+this.name+'</h2> <p> ИМЕЕТСЯ: <span id="multiply_counter">'+this.own+'</span> </p> <p> ЦЕНА: <span id="multiply_cost">'+this.cost+'</span> </p> <p> МНОЖИТЕЛЬ: <span id="multiply_factor">'+this.factor+'</span> </p> </a>';
+        document.querySelector(".b-three").innerHTML = tags;
+        
+        document.querySelector("#"+this.id).addEventListener("click", () =>
+        {
+            factor *= this.factor;
+            this.own += 1;
+            this.cost += Math.round(this.cost * 0.1);
+            this.factor += 0.2;
+            refresh();
+            document.querySelector("#"+this.id+" #multiply_counter").textContent = this.own;
+            document.querySelector("#"+this.id+" #multiply_cost").textContent = this.cost;
+            document.querySelector("#"+this.id+" #multiply_factor").textContent = this.factor;
+
+            this.update();
+        });
+    }
+    update()
+    {
+        factor = this.factor;
+    }
+}
+
+let bonusOne = new Bonus("Адин", 100, 1.1, "bonusOne");
