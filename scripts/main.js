c273abec7478c740c55706bc54b6aea7d595e4b8
Alexander <mrreads@yandex.com>
2019-09-21 12:31:36 +0300

    trying rewritte

--- scripts/main.js
+++ scripts/main.js
@@ -3,52 +3,62 @@ let factor = 1;
 
 document.querySelector('#button').addEventListener('click', function()
 {
-    counter = counter + (1 * factor);
-    refresh();
-});
-
-function refresh ()
-{
+    counter += factor;
     document.querySelector("#counter").textContent = counter;
-}
-
-refresh();
+});
 
 class Bonus
 {
-    constructor(name, cost, factor, id) 
+    constructor(name, cost, factor, id, automate) 
     {
         this.name = name;
         this.cost = cost;
         this.factor = factor;
         this.own = 0;
         this.id = id;
+        this.automate = automate;
 
         let tags = '<a class="bonus" id="'+id+'" href="#"> <h2>'+this.name+'</h2> <p> ИМЕЕТСЯ: <span id="multiply_counter">'+this.own+'</span> </p> <p> ЦЕНА: <span id="multiply_cost">'+this.cost+'</span> </p> <p> МНОЖИТЕЛЬ: <span id="multiply_factor">'+this.factor+'</span> </p> </a>';
-        document.querySelector(".b-three").innerHTML = tags;
-        
-        document.querySelector("#"+this.id).addEventListener("click", () =>
-        {
-            if (counter >= this.cost)
-            {
-                this.own += 1;
-                this.cost += Math.round(this.cost * 0.1);
-                this.factor += 0.2;
-                refresh();
-                document.querySelector("#"+this.id+" #multiply_counter").textContent = this.own;
-                document.querySelector("#"+this.id+" #multiply_cost").textContent = this.cost;
-                document.querySelector("#"+this.id+" #multiply_factor").textContent = this.factor;
+        document.querySelector(".b-three").innerHTML += tags;
 
-                this.update();
-            }
-        });
+        if (this.automate == true)
+        {
+            setInterval(() =>
+            {
+                counter += (this.own * this.factor);
+                this.refresh();    
+            }, 1 * 1000);
+        }
+
+        this.element = document.querySelector("#"+this.id);
+        this.element.addEventListener("click", () => { this.buy(); });
     }
-    update()
+
+    buy()
     {
-        counter -= this.cost;
-        factor = this.factor;
-        refresh();
+        if (counter >= this.cost)
+        {
+            counter -= this.cost;
+            factor += this.factor; 
+            this.own += 1;
+            this.factor += 0.1;
+            this.cost *= 1.2;
+            this.refresh();
+        }
+    }
+
+    refresh()
+    {
+        document.querySelector("#"+this.id+" #multiply_counter").textContent = this.own;
+        document.querySelector("#"+this.id+" #multiply_cost").textContent = this.cost;
+        document.querySelector("#"+this.id+" #multiply_factor").textContent = this.factor;
+        document.querySelector("#counter").textContent = counter;
     }
 }
 
-let bonusOne = new Bonus("Адин", 10, 1.1, "bonusOne");
+let bonusOne = new Bonus("Тыкалка 1", 10, 1.1, "bonusOne", false);
+
+let bonusTwo = new Bonus("Автомат", 20, 1, "bonusTwo", true);
+
+console.log(bonusOne.element);
+console.log(bonusTwo.element);
